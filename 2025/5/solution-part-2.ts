02a7bcf40988c656bcf1ad8f08ac6af2a5c19543
Valentin Kirilov <val.kirilov@gmail.com>
2025-12-05 07:38:14 +0200

    Solve Day 5, 2025

---
+++ 2025/5/solution-part-2.ts
@@ -0,0 +1,90 @@
+import * as fs from "fs";
+
+const argv = process.argv.slice(2);
+const inputFileName = argv[0] || "example.in";
+const input = fs.readFileSync(`${__dirname}/${inputFileName}`, "utf-8");
+const inputLines = input.split("\n");
+
+function readInput(input: string[]): {
+  ranges: [number, number][];
+  ingredients: number[];
+} {
+  const ranges: [number, number][] = [];
+  const ingredients: number[] = [];
+
+  let isReadingRanges = true;
+
+  for (const line of input) {
+    const trimmedLine = line.trim();
+
+    if (trimmedLine === "") {
+      isReadingRanges = false;
+      continue;
+    }
+
+    if (isReadingRanges) {
+      const [minStr, maxStr] = trimmedLine.split("-");
+      const min = Number(minStr);
+      const max = Number(maxStr);
+
+      ranges.push([min, max]);
+    } else {
+      const ingredient = Number(trimmedLine);
+
+      ingredients.push(ingredient);
+    }
+  }
+
+  return {
+    ranges,
+    ingredients,
+  };
+}
+
+interface Range {
+  min: number;
+  max: number;
+}
+
+function buildRanges(input: [number, number][]): Range[] {
+  const ranges: Range[] = [];
+
+  input
+    .sort(([minA], [minB]) => minA - minB)
+    .forEach(([min, max]) => {
+      if (ranges.length === 0) {
+        ranges.push({ min, max });
+        return;
+      }
+
+      for (let range of ranges) {
+        if (
+          (min >= range.min && min <= range.max) ||
+          (max >= range.min && max <= range.max)
+        ) {
+          range.min = Math.min(range.min, min);
+          range.max = Math.max(range.max, max);
+          return;
+        }
+      }
+
+      ranges.push({ min, max });
+    });
+
+  return ranges;
+}
+
+function countFreshIngredients(ranges: Range[]): number {
+  return ranges.reduce((count, range) => {
+    count += range.max - range.min + 1;
+    return count;
+  }, 0);
+}
+
+// Enough helpers, let's solve the problem
+// ----------------------------------------
+
+const { ranges: inputRanges, ingredients } = readInput(inputLines);
+const ranges = buildRanges(inputRanges);
+const freshCount = countFreshIngredients(ranges);
+console.log(freshCount);
